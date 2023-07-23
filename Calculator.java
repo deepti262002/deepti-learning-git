bbf110605a31039b294db144980ffc76d0b929cd
deepti262002 <deepti26singh26@gmail.com>
2023-07-23 22:50:14 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a=100;
+        int b=10;
+     System.out.println("addition "+ (a+b));
+     System.out.println("subtraction " + (a-b));
+     System.out.println("multiplication "+ a*b);
+     System.out.println("division"+ a/b);
+     System.out.println("modulo"+ a%b);
+     
+    
+    }
+}
